625a7aea513879bac8e632b6ceb211f8f426a509
Max <maxpete121@gmail.com>
2024-04-08 13:27:56 -0600

    iframe

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -12,58 +12,63 @@
       </div>
     </div>
     <div class="row">
-      <div class="col-10">
-        <iframe :src="finalLink" width="700px" height="500px"></iframe>
+      <button @click="pausePlay()">test Pause</button>
+      <div id="video-element" class="col-10">
+        <iframe id="video-active" :src="finalLink" width="700px" height="500px" modestbranding="0" controls="0"
+          frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen></iframe>
       </div>
     </div>
   </section>
-  </template>
-  
-  <script>
-  import { ref } from 'vue';
-  import Pop from '../utils/Pop';
-  
-  export default {
-    setup() {
-      let watchLink = ref('')
-      let finalLink = ref('')
-      async function letsWatch(){
-        let length = watchLink.value.length
-        let link = watchLink.value.slice(0, 24)
-        let linkTwo = watchLink.value.slice(32, length)
-        let draftLink = link += 'embed/'
-        let videoLink = draftLink += linkTwo
-        finalLink.value = videoLink
-        watchLink.value = ''
-        Pop.success("OH YEAH BROTHER")
-      }
-      return {
-        finalLink,
-        watchLink,
-        letsWatch
-      }
+</template>
+
+<script>
+import { ref } from 'vue';
+import Pop from '../utils/Pop';
+export default {
+  setup() {
+    let watchLink = ref('')
+    let finalLink = ref('')
+    async function letsWatch() {
+      let length = watchLink.value.length
+      let link = watchLink.value.slice(0, 24)
+      let linkTwo = watchLink.value.slice(32, length)
+      let draftLink = link += 'embed/'
+      let videoLink = draftLink += linkTwo
+      finalLink.value = videoLink
+      watchLink.value = ''
+      Pop.success("OH YEAH BROTHER")
+    }
+
+
+    return {
+      pausePlay,
+      finalLink,
+      watchLink,
+      letsWatch
     }
   }
-  </script>
-  
-  <style scoped lang="scss">
-  .home {
-    display: grid;
-    height: 80vh;
-    place-content: center;
-    text-align: center;
-    user-select: none;
-  
-    .home-card {
-      width: clamp(500px, 50vw, 100%);
-  
-      >img {
-        height: 200px;
-        max-width: 200px;
-        width: 100%;
-        object-fit: contain;
-        object-position: center;
-      }
+}
+</script>
+
+<style scoped lang="scss">
+.home {
+  display: grid;
+  height: 80vh;
+  place-content: center;
+  text-align: center;
+  user-select: none;
+
+  .home-card {
+    width: clamp(500px, 50vw, 100%);
+
+    >img {
+      height: 200px;
+      max-width: 200px;
+      width: 100%;
+      object-fit: contain;
+      object-position: center;
     }
   }
-  </style>
+}
+</style>
---
+++ client/src/services/VideoService.js
@@ -0,0 +1,7 @@
+
+
+class VideoService{
+
+}
+
+export const videoService = new VideoService()
